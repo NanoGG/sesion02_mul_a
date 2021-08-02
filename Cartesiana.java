849a23dab8ee363db8bf1758a7e05b762602a75f
NanoGG <est.daniel.munar@unimilitar.edu.co>
2021-08-02 11:21:38 -0500

    Daniel Alejandro Munar Velasquez <est.daniel.munar@unimilitar.edu.co> primer cambio

---
+++ Cartesiana.java
@@ -0,0 +1,44 @@
+
+package sesion02_mul_a;
+
+public class Cartesiana {
+    private float x;
+    private float y;
+
+    public Cartesiana(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Cartesiana() {
+    }
+
+    public Cartesiana polar_cartesiano(float radio, float angulo) {
+        float x = radio * (float)Math.cos(angulo);
+        float y = radio * (float)Math.sin(angulo);
+        return new Cartesiana(x, y);
+    }
+    
+    public Polar cartesiano_polar(float x, float y) {
+        float radio =(float) math.sqrt((math.pow(x,2))+(math.pow(y,2)));
+        float angulo =(float) math.atan(y/x);
+        return new Polar(radio, angulo);
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+}
---
+++ Sesion02_mul_a.java
@@ -0,0 +1,9 @@
+package sesion02_mul_a;
+
+public class Sesion02_mul_a {
+
+    public static void main(String[] args) {
+        Cartesiana a, b, c= new Cartesiana(6, 11);
+    }
+    
+}
